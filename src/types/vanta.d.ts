5d322eaf86bc9706b14938a9dcb27f8717034539
fauzi niko <fauziniko12@gmail.com>
2025-05-17 20:14:22 +0700

    [Misc] Update: package-lock.json,package.json,src/app/globals.css,src/app/page.tsx,src/types/vanta.d.ts,

---
+++ src/types/vanta.d.ts
@@ -0,0 +1,5 @@
+declare module 'vanta/dist/vanta.birds.min' {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const VANTA: (options: Record<string, unknown>) => unknown;
+  export default VANTA;
+}
