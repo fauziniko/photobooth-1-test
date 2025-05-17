d9ba6210262b0b906f28c69ee8279a81b543aea6
fauzi niko <fauziniko12@gmail.com>
2025-05-17 18:22:57 +0700

    [Misc] Update: src/types/gif.js.d.ts,

--- src/types/gif.js.d.ts
+++ src/types/gif.js.d.ts
@@ -1,4 +1,9 @@
 declare module 'gif.js' {
-  const GIF: any;
+  class GIF {
+    constructor(options?: object);
+    addFrame(image: HTMLCanvasElement | HTMLImageElement, options?: object): void;
+    on(event: string, callback: (blob: Blob) => void): void;
+    render(): void;
+  }
   export default GIF;
 }
