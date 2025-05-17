13557a2967d471a12ba655a624dc15ea4d09177f
fauzi niko <fauziniko12@gmail.com>
2025-05-17 12:04:16 +0700

    [Misc] Update: components/Camera.tsx,

--- components/Camera.tsx
+++ components/Camera.tsx
@@ -72,7 +72,7 @@ export default function Camera({ onCapture, countdown, photosToTake, onStartCapt
             position: 'absolute',
             top: 0, left: 0, width: '100%', height: '100%',
             display: 'flex', alignItems: 'center', justifyContent: 'center',
-            fontSize: 80, fontWeight: 'bold', color: '#111', background: 'rgba(255,255,255,0.5)'
+            fontSize: 80, fontWeight: 'bold', color: '#fff', background: 'rgba(0,0,0,0.4)'
           }}>
             {count}
           </div>
