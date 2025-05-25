b82bedc36bd4587b560551920c249dd7cba80c37
fauzi niko <fauziniko12@gmail.com>
2025-05-26 00:30:21 +0700

    [Misc] Update: src/app/page.tsx,

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -272,12 +272,6 @@ export default function Home() {
           <div style={{ marginTop: 16, color: '#888' }}>
             {photos.length > 0 && `Foto diambil: ${photos.length} / ${layout}`}
           </div>
-          <Camera
-            onCapture={handleCapture}
-            photosToTake={layout}
-            countdown={countdown}
-            onStartCapture={handleStartCapture}
-          />
         </>
       ) : (
         <div className="strip-controls-wrapper">
